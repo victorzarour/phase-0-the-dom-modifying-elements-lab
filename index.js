14b884baae56e3e69486f1d2381f1294bba7eadd
Victor <vx_zz@hotmail.com>
2022-05-24 12:54:19 -0400

    commit tree 8af00c8445b8ce2de559ef2a8574f88c342e2764

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "YOUR-NAME is the champion";
